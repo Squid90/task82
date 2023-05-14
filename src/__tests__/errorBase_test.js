d6ef885ce1fcd38af3ca895432400858898bd7ed
Squid90 <squid90@rambler.ru>
2023-05-14 20:25:02 +0300

    Done

--- src/__tests__/errorBase_test.js
+++ src/__tests__/errorBase_test.js
@@ -2,11 +2,11 @@ import ErrorRepository from '../errorBase';
 
 test('Проверка кода ошибки', () => {
   const expectedDescription = 'Error #1';
-  const currentDescription = new ErrorRepository(111, 'Error #1');
+  const currentDescription = new ErrorRepository();
   expect(expectedDescription).toEqual(currentDescription.translate(111));
 });
 
 test('Проверка ошибки при неизвестном коде', () => {
-  const currentDescription = new ErrorRepository(111, 'Error #1');
+  const currentDescription = new ErrorRepository();
   expect(() => currentDescription.translate(222)).toThrow('Unknown error');
 });
--- src/errorBase.js
+++ src/errorBase.js
@@ -1,18 +1,15 @@
 const errorBase = new Map();
 export default class ErrorRepository {
-  constructor(code, description) {
-    this.code = code;
-    this.description = description;
-
-    errorBase.set(this, {
-      code,
-      description,
-    });
+  constructor() {
+    errorBase.set(this, 
+      [111,
+        'Error #1']
+    );
   }
 
   translate(code) {
-    if (errorBase.get(this).code === code) {
-      return errorBase.get(this).description;
+    if (errorBase.get(this)[0] === code) {
+      return errorBase.get(this)[1];
     }
     throw new Error('Unknown error');
   }
