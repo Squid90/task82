4d34d723275e6846ab4090642b0135a20cf5efdb
Squid90 <squid90@rambler.ru>
2023-05-16 22:06:00 +0300

    done

--- src/errorBase.js
+++ src/errorBase.js
@@ -1,15 +1,16 @@
-const errorBase = new Map();
 export default class ErrorRepository {
   constructor() {
-    errorBase.set(this, 
+    this.errorBase = new Map();
+    this.errorBase.set(
+      this,
       [111,
-        'Error #1']
+        'Error #1'],
     );
   }
 
   translate(code) {
-    if (errorBase.get(this)[0] === code) {
-      return errorBase.get(this)[1];
+    if (this.errorBase.get(this)[0] === code) {
+      return this.errorBase.get(this)[1];
     }
     throw new Error('Unknown error');
   }
